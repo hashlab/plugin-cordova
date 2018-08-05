640ad2ce46e0f00a160e423b0ec1442ec16e5ba2
João Gabriel F Miranda <faria.joaog@gmail.com>
2018-08-05 19:54:41 -0300

    Fix new version

--- src/android/StoneSDK.java
+++ src/android/StoneSDK.java
@@ -31,7 +31,6 @@ import stone.providers.CancellationProvider;
 import stone.providers.LoadTablesProvider;
 import stone.providers.SendEmailProvider;
 import stone.providers.TransactionProvider;
-import stone.providers.DownloadTablesProvider;
 import stone.user.UserModel;
 import stone.utils.EmailClient;
 import stone.utils.GlobalInformations;
@@ -385,31 +384,30 @@ public class StoneSDK extends CordovaPlugin {
     }
 
     private void tablesDownload(final CallbackContext callbackContext) throws JSONException {
-        /* callbackContext.success("Nao ha tabelas para baixar, elas ja estao atualizadas.");       */
+        callbackContext.success("Nao ha tabelas para baixar, elas ja estao atualizadas.");      
 
         // IMPORTANTE: Mantenha esse provider na sua MAIN, pois ele ira baixar as tabelas AIDs e CAPKs dos servidores da Stone e sera utilizada quando necessario.
-        ApplicationCache applicationCache = new ApplicationCache(StoneSDK.this.cordova.getActivity());
-        if (!applicationCache.checkIfHasTables()) {
-            // Realiza processo de download das tabelas em sua totalidade.
-            final DownloadTablesProvider downloadTablesProvider = new DownloadTablesProvider(StoneSDK.this.cordova.getActivity(), Stone.getUserModel(0));
-            downloadTablesProvider.setDialogMessage("Baixando as tabelas, por favor aguarde");
-            downloadTablesProvider.setWorkInBackground(false); // para dar feedback ao usuario ou nao.
-            downloadTablesProvider.setConnectionCallback(new StoneCallbackInterface() {
-                public void onSuccess() {
-                    Toast.makeText(StoneSDK.this.cordova.getActivity(), "Tabelas baixadas com sucesso.", Toast.LENGTH_SHORT).show();
-                    callbackContext.success("Tabelas baixadas com sucesso.");
-                }
-
-                public void onError() {
-                    Toast.makeText(StoneSDK.this.cordova.getActivity(), "Erro ao baixar tabelas.", Toast.LENGTH_SHORT).show();
-                    callbackContext.error(downloadTablesProvider.getListOfErrors().toString());
-                }
-            });
-            downloadTablesProvider.execute();
-        } else {
-            callbackContext.success("Nao ha tabelas para baixar, elas ja estao atualizadas.");
-        }
-
+        /* ApplicationCache applicationCache = new ApplicationCache(StoneSDK.this.cordova.getActivity()); */
+        /* if (!applicationCache.checkIfHasTables()) { */
+        /*     // Realiza processo de download das tabelas em sua totalidade. */
+        /*     final DownloadTablesProvider downloadTablesProvider = new DownloadTablesProvider(StoneSDK.this.cordova.getActivity(), Stone.getUserModel(0)); */
+        /*     downloadTablesProvider.setDialogMessage("Baixando as tabelas, por favor aguarde"); */
+        /*     downloadTablesProvider.setWorkInBackground(false); // para dar feedback ao usuario ou nao. */
+        /*     downloadTablesProvider.setConnectionCallback(new StoneCallbackInterface() { */
+        /*         public void onSuccess() { */
+        /*             Toast.makeText(StoneSDK.this.cordova.getActivity(), "Tabelas baixadas com sucesso.", Toast.LENGTH_SHORT).show(); */
+        /*             callbackContext.success("Tabelas baixadas com sucesso."); */
+        /*         } */
+        /*  */
+        /*         public void onError() { */
+        /*             Toast.makeText(StoneSDK.this.cordova.getActivity(), "Erro ao baixar tabelas.", Toast.LENGTH_SHORT).show(); */
+        /*             callbackContext.error(downloadTablesProvider.getListOfErrors().toString()); */
+        /*         } */
+        /*     }); */
+        /*     downloadTablesProvider.execute(); */
+        /* } else { */
+        /*     callbackContext.success("Nao ha tabelas para baixar, elas ja estao atualizadas."); */
+        /* } */
     }
 
     private void tablesUpdate(final CallbackContext callbackContext) throws JSONException {
